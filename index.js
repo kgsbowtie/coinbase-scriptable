d7fd1c2cd9d24410c056ae73ca1e3017f81e36fa
Kyle Sandars <sushomeru@yahoo.com>
2021-08-23 13:58:21 -0500

    Transpiled index and lib to ES6

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-var Account       = importModule('./lib/model/Account.js'),
-    Address       = importModule('./lib/model/Address.js'),
-    Buy           = importModule('./lib/model/Buy.js'),
-    Checkout      = importModule('./lib/model/Checkout.js'),
-    Client        = importModule('./lib/Client.js'),
-    Deposit       = importModule('./lib/model/Deposit.js'),
-    Merchant      = importModule('./lib/model/Merchant.js'),
-    Notification  = importModule('./lib/model/Notification.js'),
-    Order         = importModule('./lib/model/Order.js'),
-    PaymentMethod = importModule('./lib/model/PaymentMethod.js'),
-    Sell          = importModule('./lib/model/Sell.js'),
-    Transaction   = importModule('./lib/model/Transaction.js'),
-    User          = importModule('./lib/model/User.js'),
-    Withdrawal    = importModule('./lib/model/Withdrawal.js');
+const Account       = importModule('./lib/model/Account.js');
+const Address       = importModule('./lib/model/Address.js');
+const Buy           = importModule('./lib/model/Buy.js');
+const Checkout      = importModule('./lib/model/Checkout.js');
+const Client        = importModule('./lib/Client.js');
+const Deposit       = importModule('./lib/model/Deposit.js');
+const Merchant      = importModule('./lib/model/Merchant.js');
+const Notification  = importModule('./lib/model/Notification.js');
+const Order         = importModule('./lib/model/Order.js');
+const PaymentMethod = importModule('./lib/model/PaymentMethod.js');
+const Sell          = importModule('./lib/model/Sell.js');
+const Transaction   = importModule('./lib/model/Transaction.js');
+const User          = importModule('./lib/model/User.js');
+const Withdrawal    = importModule('./lib/model/Withdrawal.js');
 
-var model = {
+const model = {
   'Account'       : Account,
   'Address'       : Address,
   'Buy'           : Buy,
